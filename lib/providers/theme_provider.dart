60c805bf80ffcd9f8dd246a570b16a434510d2d9
Akshit Saxena <34300558+Akshiiitsaxena@users.noreply.github.com>
2022-11-29 21:31:41 +0530

    fix: read system theme on app start

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 @immutable
 class ThemeState {
   final ThemeMode mode;
 
-  const ThemeState({this.mode = ThemeMode.light});
+  const ThemeState(this.mode);
 
-  ThemeState copyWith({ThemeMode? mode}) => ThemeState(mode: mode ?? this.mode);
+  ThemeState copyWith({ThemeMode? mode}) => ThemeState(mode ?? this.mode);
 }
 
 class ThemeStateNotifier extends StateNotifier<ThemeState> {
-  ThemeStateNotifier() : super(const ThemeState());
+  ThemeStateNotifier()
+      : super(ThemeState(
+          SchedulerBinding.instance.window.platformBrightness ==
+                  Brightness.light
+              ? ThemeMode.light
+              : ThemeMode.dark,
+        ));
 
   void setTheme(ThemeMode value) => state = state.copyWith(mode: value);
 }
